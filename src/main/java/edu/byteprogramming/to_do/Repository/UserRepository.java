a3237735736a0c161b5b14b78aaf0ddb4b561e56
Santiago <santiago.carmona-p@mail.escuelaing.edu.co>
2025-06-25 13:16:31 -0500

    Revert "Se añadió implementación a controladores y servicios."
    This reverts commit df2e31f6cdb9f33b59d5d52b7dc440d67364a29d.

--- src/main/java/edu/byteprogramming/to_do/Repository/UserRepository.java
+++ src/main/java/edu/byteprogramming/to_do/Repository/UserRepository.java
@@ -2,9 +2,8 @@ package edu.byteprogramming.to_do.Repository;
 
 import edu.byteprogramming.to_do.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
+    
 }
