f5726bdbaf5c3c7a2e1905487b12a89b61658258
Santiago <santiago.carmona-p@mail.escuelaing.edu.co>
2025-06-25 13:21:22 -0500

    Se añadió implementación a controladores y servicios. Se añadió código a la clase AuthController.java para poder recibir peticiones desde el front y validar que un usuario no trate de crear una cuenta con un correo que ya está registrado.
    Adicionalmente, se definió el método findByEmail en UserRepository.java para poder buscar si ese correo está registrado en la base de datos.
    
    Esta implementación es un poco genérica, falta ajustar detalles en el front para hacer la petición correctamente y así poder recibirla en el backend y hacer las validaciones necesarias.

--- src/main/java/edu/byteprogramming/to_do/Controller/AuthController.java
+++ src/main/java/edu/byteprogramming/to_do/Controller/AuthController.java
@@ -1,5 +1,24 @@
 package edu.byteprogramming.to_do.Controller;
 
-public class AuthController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import edu.byteprogramming.to_do.Repository.UserRepository;
+import edu.byteprogramming.to_do.Model.User;
+import java.util.Optional;
 
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public String register(@RequestParam String email) {
+        Optional<User> existingUser = userRepository.findByEmail(email);
+        if (existingUser.isPresent()) {
+            return "Email ya registrado";
+        }
+
+        return "Registro permitido";
+    }
 }
